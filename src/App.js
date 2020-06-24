7bf6885700d86385cbe5485690c60642d85776bb
David Yu <dyu@esri.com>
2020-06-23 22:22:37 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React bap pab bap
+          bing bing bang feature branch
         </a>
       </header>
     </div>
